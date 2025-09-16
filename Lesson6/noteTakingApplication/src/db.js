2e42b79bbcb403f47b1c9dbbfbc78de96c4f6e50
Dipen Parihar <parihar.dipen@gmail.com>
2025-09-16 19:07:07 -0400

    Db.json

--- Lesson6/noteTakingApplication/src/db.js
+++ Lesson6/noteTakingApplication/src/db.js
@@ -3,22 +3,31 @@ const fs = require('fs/promises');
 const DB_PATH = './data/db.json';
 // Get Data from db
 function getData() {
+  // What is happening over here ?
   return fs.readFile(DB_PATH);
 }
 function saveDB(data) {
+  // 4. Function get triggered
   return fs.writeFile(DB_PATH, JSON.stringify(data, null, 2));
+}
+function parseData(databaseData, note) {
+  console.log('databaseData -- ', databaseData);
+  // 3. Funtion get triggered
+  var db = JSON.parse(databaseData);
+  db.notes.push(note);
+  return db;
 }
 function insert(note) {
   if (note == '') {
     throw new Error('Data is null');
   }
+  // 2. Function is triggered
   return getData()
-    .then(function (databaseData) {
-      var db = JSON.parse(databaseData);
-      db.notes.push(note);
-      return db;
+    .then(function (data) {
+      return parseData(data, note);
     })
     .then(saveDB);
 }
-insert('');
+// 1. Function I am triggering
+insert('New notes notes');
 module.exports = { insert, getData };
